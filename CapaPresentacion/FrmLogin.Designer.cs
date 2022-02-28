c8920e367656aa88976b93c2f36a2f1c1433eaa2
asier_elorza <asierelorza18@gmail.com>
2022-02-28 17:00:09 +0100

    Cambio Base de datos, datetime --> datetime2

--- CapaPresentacion/FrmLogin.Designer.cs
+++ CapaPresentacion/FrmLogin.Designer.cs
@@ -48,56 +48,56 @@ namespace CapaPresentacion
             this.label7 = new System.Windows.Forms.Label();
             this.txtSignNumCuenta = new System.Windows.Forms.TextBox();
             this.grpLogin = new System.Windows.Forms.GroupBox();
+            this.linkSignup = new System.Windows.Forms.LinkLabel();
             this.grpSignup = new System.Windows.Forms.GroupBox();
             this.linkLogin = new System.Windows.Forms.LinkLabel();
-            this.linkSignup = new System.Windows.Forms.LinkLabel();
             this.grpLogin.SuspendLayout();
             this.grpSignup.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtEmail
             // 
-            this.txtEmail.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtEmail.Location = new System.Drawing.Point(93, 101);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(178, 23);
+            this.txtEmail.Size = new System.Drawing.Size(178, 22);
             this.txtEmail.TabIndex = 0;
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEmail.Location = new System.Drawing.Point(8, 104);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(38, 15);
+            this.lblEmail.Size = new System.Drawing.Size(42, 16);
             this.lblEmail.TabIndex = 1;
             this.lblEmail.Text = "Email";
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPassword.Location = new System.Drawing.Point(8, 150);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(72, 15);
+            this.lblPassword.Size = new System.Drawing.Size(77, 16);
             this.lblPassword.TabIndex = 3;
             this.lblPassword.Text = "Contraseña";
             // 
             // txtPass
             // 
-            this.txtPass.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPass.Location = new System.Drawing.Point(93, 147);
             this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(178, 23);
+            this.txtPass.Size = new System.Drawing.Size(178, 22);
             this.txtPass.TabIndex = 2;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Bold);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
             this.label4.Location = new System.Drawing.Point(75, 37);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(146, 23);
+            this.label4.Size = new System.Drawing.Size(150, 24);
             this.label4.TabIndex = 6;
             this.label4.Text = "INICIA SESIÓN";
             // 
@@ -110,7 +110,7 @@ namespace CapaPresentacion
             this.btnLogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Indigo;
             this.btnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Indigo;
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogin.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             this.btnLogin.ForeColor = System.Drawing.SystemColors.Control;
             this.btnLogin.Location = new System.Drawing.Point(79, 224);
             this.btnLogin.Name = "btnLogin";
@@ -129,7 +129,7 @@ namespace CapaPresentacion
             this.btnSignup.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Indigo;
             this.btnSignup.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Indigo;
             this.btnSignup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSignup.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Bold);
+            this.btnSignup.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             this.btnSignup.ForeColor = System.Drawing.SystemColors.Control;
             this.btnSignup.Location = new System.Drawing.Point(83, 313);
             this.btnSignup.Name = "btnSignup";
@@ -142,102 +142,102 @@ namespace CapaPresentacion
             // lblRegistrarse
             // 
             this.lblRegistrarse.AutoSize = true;
-            this.lblRegistrarse.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Bold);
+            this.lblRegistrarse.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
             this.lblRegistrarse.Location = new System.Drawing.Point(91, 37);
             this.lblRegistrarse.Name = "lblRegistrarse";
-            this.lblRegistrarse.Size = new System.Drawing.Size(128, 23);
+            this.lblRegistrarse.Size = new System.Drawing.Size(139, 24);
             this.lblRegistrarse.TabIndex = 14;
             this.lblRegistrarse.Text = "REGÍSTRATE";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(14, 172);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 15);
+            this.label2.Size = new System.Drawing.Size(122, 16);
             this.label2.TabIndex = 13;
             this.label2.Text = "Repetir contraseña";
             // 
             // txtSignRepeatPass
             // 
-            this.txtSignRepeatPass.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSignRepeatPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtSignRepeatPass.Location = new System.Drawing.Point(129, 169);
             this.txtSignRepeatPass.Name = "txtSignRepeatPass";
-            this.txtSignRepeatPass.Size = new System.Drawing.Size(148, 23);
+            this.txtSignRepeatPass.Size = new System.Drawing.Size(148, 22);
             this.txtSignRepeatPass.TabIndex = 12;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(14, 126);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 15);
+            this.label5.Size = new System.Drawing.Size(77, 16);
             this.label5.TabIndex = 11;
             this.label5.Text = "Contraseña";
             // 
             // txtSignPass
             // 
-            this.txtSignPass.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSignPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtSignPass.Location = new System.Drawing.Point(99, 123);
             this.txtSignPass.Name = "txtSignPass";
-            this.txtSignPass.Size = new System.Drawing.Size(178, 23);
+            this.txtSignPass.Size = new System.Drawing.Size(178, 22);
             this.txtSignPass.TabIndex = 10;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(14, 80);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.Size = new System.Drawing.Size(42, 16);
             this.label6.TabIndex = 9;
             this.label6.Text = "Email";
             // 
             // txtSignMail
             // 
-            this.txtSignMail.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSignMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtSignMail.Location = new System.Drawing.Point(99, 77);
             this.txtSignMail.Name = "txtSignMail";
-            this.txtSignMail.Size = new System.Drawing.Size(178, 23);
+            this.txtSignMail.Size = new System.Drawing.Size(178, 22);
             this.txtSignMail.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(14, 219);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.Size = new System.Drawing.Size(62, 16);
             this.label3.TabIndex = 17;
             this.label3.Text = "Teléfono";
             // 
             // txtSignTelefono
             // 
-            this.txtSignTelefono.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSignTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtSignTelefono.Location = new System.Drawing.Point(99, 216);
             this.txtSignTelefono.Name = "txtSignTelefono";
-            this.txtSignTelefono.Size = new System.Drawing.Size(178, 23);
+            this.txtSignTelefono.Size = new System.Drawing.Size(178, 22);
             this.txtSignTelefono.TabIndex = 16;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(14, 266);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(94, 15);
+            this.label7.Size = new System.Drawing.Size(99, 16);
             this.label7.TabIndex = 19;
             this.label7.Text = "Numero cuenta";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // txtSignNumCuenta
             // 
-            this.txtSignNumCuenta.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSignNumCuenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtSignNumCuenta.Location = new System.Drawing.Point(114, 263);
             this.txtSignNumCuenta.Name = "txtSignNumCuenta";
-            this.txtSignNumCuenta.Size = new System.Drawing.Size(163, 23);
+            this.txtSignNumCuenta.Size = new System.Drawing.Size(163, 22);
             this.txtSignNumCuenta.TabIndex = 18;
             // 
             // grpLogin
@@ -256,6 +256,17 @@ namespace CapaPresentacion
             this.grpLogin.TabStop = false;
             this.grpLogin.Text = "INICIA SESIÓN";
             // 
+            // linkSignup
+            // 
+            this.linkSignup.AutoSize = true;
+            this.linkSignup.Location = new System.Drawing.Point(119, 272);
+            this.linkSignup.Name = "linkSignup";
+            this.linkSignup.Size = new System.Drawing.Size(60, 13);
+            this.linkSignup.TabIndex = 23;
+            this.linkSignup.TabStop = true;
+            this.linkSignup.Text = "Registrarse";
+            this.linkSignup.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkSignup_LinkClicked);
+            // 
             // grpSignup
             // 
             this.grpSignup.Controls.Add(this.linkLogin);
@@ -271,7 +282,7 @@ namespace CapaPresentacion
             this.grpSignup.Controls.Add(this.btnSignup);
             this.grpSignup.Controls.Add(this.txtSignRepeatPass);
             this.grpSignup.Controls.Add(this.label2);
-            this.grpSignup.Location = new System.Drawing.Point(341, 12);
+            this.grpSignup.Location = new System.Drawing.Point(23, 12);
             this.grpSignup.Name = "grpSignup";
             this.grpSignup.Size = new System.Drawing.Size(290, 379);
             this.grpSignup.TabIndex = 21;
@@ -289,23 +300,12 @@ namespace CapaPresentacion
             this.linkLogin.Text = "Ya tengo una cuenta";
             this.linkLogin.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLogin_LinkClicked);
             // 
-            // linkSignup
-            // 
-            this.linkSignup.AutoSize = true;
-            this.linkSignup.Location = new System.Drawing.Point(119, 272);
-            this.linkSignup.Name = "linkSignup";
-            this.linkSignup.Size = new System.Drawing.Size(60, 13);
-            this.linkSignup.TabIndex = 23;
-            this.linkSignup.TabStop = true;
-            this.linkSignup.Text = "Registrarse";
-            this.linkSignup.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkSignup_LinkClicked);
-            // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(658, 412);
+            this.ClientSize = new System.Drawing.Size(338, 412);
             this.Controls.Add(this.grpSignup);
             this.Controls.Add(this.grpLogin);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));

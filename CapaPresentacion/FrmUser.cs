dd522c65584706b1158504a4c35eed338f794a8b
asier_elorza <asierelorza18@gmail.com>
2022-03-01 13:16:45 +0100

    Revert "Revert "Modificar usuario Frm y gestor""
    This reverts commit ce2ef4444484623947ea08ab04e44d29ffbf6e18.

--- CapaPresentacion/FrmUser.cs
+++ CapaPresentacion/FrmUser.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Entidades;
 
 namespace CapaPresentacion
 {
@@ -17,5 +18,67 @@ namespace CapaPresentacion
             InitializeComponent();
         }
 
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if(txtEmail.Text.Equals("") || txtTelefono.Text.Equals("") || txtNumCuenta.Text.Equals(""))
+            {
+                MessageBox.Show("No puede haber datos vacíos.");
+                return;
+            }
+            if (!int.TryParse(txtTelefono.Text, out int telefonoInt) || txtTelefono.Text.Length != 9)
+            {
+                MessageBox.Show("Número de teléfono inválido");
+                return;
+            }
+            if (txtNumCuenta.Text.Length != 20)
+            {
+                MessageBox.Show("Número de cuenta inválido");
+                return;
+            }
+            if (chkCambiarPass.Checked)
+            {
+                if(txtContrasena.Text.Equals("") || txtRepContrasena.Text.Equals("") || !txtContrasena.Text.Equals(txtRepContrasena.Text))
+                {
+                    MessageBox.Show("Contraseñas no coinciden.");
+                    return;
+                }
+                Usuario user = Program.gestor.ModificarUsuario(Program.userActive.id, txtEmail.Text, telefonoInt, txtNumCuenta.Text, txtContrasena.Text, txtRepContrasena.Text, out string error);
+                if(user == null)
+                {
+                    MessageBox.Show(error);
+                    return;
+                }
+                Program.userActive = user;
+                
+            }
+            Usuario user2 = Program.gestor.ModificarUsuario(Program.userActive.id, txtEmail.Text, telefonoInt, txtRepContrasena.Text, out string error2);
+        }
+
+        private void FrmUser_Load(object sender, EventArgs e)
+        {
+            chkCambiarPass.Checked = false;
+            txtEmail.Text = Program.userActive.email;
+            txtTelefono.Text = Program.userActive.telefono.ToString();
+            txtNumCuenta.Text = Program.userActive.numCuenta;
+            txtContrasena.PasswordChar = '*';
+            txtRepContrasena.PasswordChar = '*';
+        }
+
+        private void chkCambiarPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkCambiarPass.Checked)
+            {
+                grpCambiarPass.Enabled = true;
+            }
+            else
+            {
+                grpCambiarPass.Enabled = false;
+            }
+        }
     }
 }
